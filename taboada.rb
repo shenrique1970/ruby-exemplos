8a1b31f4c059ca26d78de19cbb74ebb08a21a3d7
shenrique1970 <shvribeiro@hotmail.com>
2025-12-29 10:59:44 -0300

    revisão taboada.

--- taboada.rb
+++ taboada.rb
@@ -13,14 +13,22 @@
 #taboada(4,2)
 
 # outra forma de se fazer
-def taboada(t, limite = 10) 
+# Cria uma função chamada taboada.
+# Recebe dois parâmetros:
+# t: o número da tabuada.
+# limite: até onde a tabuada vai (por padrão é 10, se não for informado).
+def taboada(t, limite = 10)
+  # Titulo da taboada. 
   puts "Taboada de #{t}" 
+  # Loop para gerar os cálculos
   (1..limite).each do |i| 
     puts "#{t} x #{i} = #{t * i}"
-  end 
+  end
+  # salta uma linha a cada ieração.
   puts "\n" 
 end
 
+# chama a função
 taboada(2) # até 10 
 taboada(3, 5) # até 5 
 taboada(4, 12) # até 12
