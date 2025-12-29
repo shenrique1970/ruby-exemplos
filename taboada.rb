3021653b9f7b720f3c2bd6ba44e77e3625d7f3b9
shenrique1970 <shvribeiro@hotmail.com>
2025-12-29 10:41:16 -0300

    amplição

--- taboada.rb
+++ taboada.rb
@@ -1,13 +1,26 @@
 # taboada
 
-def taboada(t, i)
-  puts "Taboada de #{t}"
-  for i in 1..10
+# def taboada(t, i)
+#  puts "Taboada de #{t}"
+#  for i in 1..10
+#   puts "#{t} x #{i} = #{t * i}"
+#  end
+#  puts "\n"
+#end
+
+#taboada(2,2)
+#taboada(3,2)
+#taboada(4,2)
+
+
+def taboada(t, limite = 10) 
+  puts "Taboada de #{t}" 
+  (1..limite).each do |i| 
     puts "#{t} x #{i} = #{t * i}"
-  end
-  puts "\n"
+  end 
+  puts "\n" 
 end
 
-taboada(2,2)
-taboada(3,2)
-taboada(4,2)
+taboada(2) # até 10 
+taboada(3, 5) # até 5 
+taboada(4, 12) # até 12
